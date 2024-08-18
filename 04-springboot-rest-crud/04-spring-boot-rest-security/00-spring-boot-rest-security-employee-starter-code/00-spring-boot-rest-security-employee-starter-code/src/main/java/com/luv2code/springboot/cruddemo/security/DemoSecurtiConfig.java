b4a031e476eeca9cbf705de31ca1f9c1dc35dc06
Yatindra Mahadik <mahadik.yatindra@gmail.com>
2024-08-18 16:14:36 -0400

    Configuring roles and restricting access

--- 04-springboot-rest-crud/04-spring-boot-rest-security/00-spring-boot-rest-security-employee-starter-code/00-spring-boot-rest-security-employee-starter-code/src/main/java/com/luv2code/springboot/cruddemo/security/DemoSecurtiConfig.java
+++ 04-springboot-rest-crud/04-spring-boot-rest-security/00-spring-boot-rest-security-employee-starter-code/00-spring-boot-rest-security-employee-starter-code/src/main/java/com/luv2code/springboot/cruddemo/security/DemoSecurtiConfig.java
@@ -3,9 +3,13 @@ package com.luv2code.springboot.cruddemo.security;
 import org.springframework.beans.factory.annotation.Configurable;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class DemoSecurtiConfig {
@@ -21,14 +25,32 @@ public class DemoSecurtiConfig {
         UserDetails pradnya = User.builder()
                 .username("pradnya")
                 .password("{noop}test123")
-                .roles("Employee", "MANAGER")
+                .roles("EMPLOYEE", "MANAGER")
                 .build();
 
         UserDetails ashwini = User.builder()
                 .username("ashwini")
                 .password("{noop}test123")
-                .roles("Employee", "MANAGER", "ADMIN")
+                .roles("EMPLOYEE", "MANAGER", "ADMIN")
                 .build();
         return new InMemoryUserDetailsManager(yatin, pradnya, ashwini);
     }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity.authorizeHttpRequests(configurer ->
+                configurer
+                        .requestMatchers(HttpMethod.GET, "/api/employees").hasRole("EMPLOYEE")
+                        .requestMatchers(HttpMethod.GET, "/api/employees/**").hasRole("EMPLOYEE")
+                        .requestMatchers(HttpMethod.POST, "/api/employees").hasRole("MANAGER")
+                        .requestMatchers(HttpMethod.PUT, "/api/employees").hasRole("MANAGER")
+                        .requestMatchers(HttpMethod.DELETE, "/api/employees/**").hasRole("ADMIN")
+        );
+
+        httpSecurity.httpBasic(Customizer.withDefaults());
+
+        httpSecurity.csrf(csrf -> csrf.disable());
+        return httpSecurity.build();
+    }
+
 }
