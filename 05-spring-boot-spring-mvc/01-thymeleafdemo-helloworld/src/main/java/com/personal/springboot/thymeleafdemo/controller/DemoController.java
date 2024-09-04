e987152d1533420fcd9d36aaa288f4339cc2c13e
Yatindra Mahadik <55409963+asce1001@users.noreply.github.com>
2024-09-03 23:44:42 -0400

    code cleanup

--- 05-spring-boot-spring-mvc/01-thymeleafdemo-helloworld/src/main/java/com/personal/springboot/thymeleafdemo/controller/DemoController.java
+++ 05-spring-boot-spring-mvc/01-thymeleafdemo-helloworld/src/main/java/com/personal/springboot/thymeleafdemo/controller/DemoController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class DemoController {
 
-    // mapping for hello
+    
     @GetMapping("/hello")
     public String sayHello(Model model){
         model.addAttribute("theDate",java.time.LocalDateTime.now());
